663d50b1169481e3111a7cfef307d375783b0b50
LOLMan707 <doruktekbaser66@gmail.com>
2022-07-12 03:16:44 +0300

    scrap everything, begin from scratch
    i hate having fun

--- astro.config.mjs
+++
@@ -1,8 +0,0 @@
-import { defineConfig } from 'astro/config';
-import partytown from '@astrojs/partytown';
-import sitemap from '@astrojs/sitemap';
-
-// https://astro.build/config
-export default defineConfig({
-  integrations: [partytown(), sitemap()],
-});
